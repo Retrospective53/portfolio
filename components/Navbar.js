beec49bfddb34ad06cb7b19cf98dfa9f698ea52b
Retrospective53 <harry.pseu53@gmail.com>
2023-02-22 01:27:55 +0700

    navbar aiOutlineClose

--- components/Navbar.js
+++ components/Navbar.js
@@ -41,17 +41,18 @@ const Nabvar = () => {
         </div>
       </div>
       <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"></div>
-        <div className="flex w-full items-center justify-between">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
           <div>
-            <Image
-              src="/../public/assets/navLogo.png"
-              width="87"
-              height="35"
-              alt="/"
-            />
-            <div>
-              <AiOutlineClose />
+            <div className="flex w-full items-center justify-between">
+              <Image
+                src="/../public/assets/navLogo.png"
+                width="87"
+                height="35"
+                alt="/"
+              />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
             </div>
           </div>
         </div>
